0d50175ad26744da30fe779d4d2e4d59e0caee16
tanvir244 <tr30365s25@gmail.com>
2024-06-03 08:12:52 +0600

    Navbar and Banner section complete already

---
+++ src/pages/Home/Home/Home.jsx
@@ -0,0 +1,13 @@
+import Navbar from "../../Shared/Navbar/Navbar";
+import Banner from "../Banner/Banner";
+
+const Home = () => {
+    return (
+        <div>
+            <Navbar></Navbar>
+            <Banner></Banner>
+        </div>
+    );
+};
+
+export default Home;
